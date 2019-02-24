2fe2591a11033353892e492088c6e9a0b57346d4
G0_1 <zyxjxl.3@gmail.com>
2019-02-25 00:10:24 +0800

    modified:   server.go

--- server.go
+++ server.go
@@ -1,30 +1,50 @@
 package detector
 
-import ()
+import (
+	"net/http"
+)
 
 type Server struct {
 	Dr *Detector
 
 	Version int64
 
-	Host      string
+	Host string
+	Port string
+
 	Peers     map[string]int64
 	PeerCount int64
 }
 
-func NewSever(host string, peers map[string]int64, version int64) (s *Server) {
+func NewSever(host, port string, peers map[string]int64) (s *Server) {
 	s = &Server{
 		Host:      host,
+		Port:      port,
 		Peers:     peers,
 		PeerCount: len(peers),
-		Version:   version,
 	}
 
 	return
 }
 
-func (s *Server) Start() {
+func (s *Server) Start(isFirst bool) {
+	go s.serve()
 
+	if isFirst {
+		s.Dr = NewDetector(s.Version + 1)
+
+		s.TransferDetector()
+	}
+}
+
+func (s *Server) serve() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		s.ReceiveDetector()
+		s.TransferDetector()
+	}))
+
+	http.ListenAndServe(s.Host+s.Port, mux)
 }
 
 func (s *Server) ReceiveDetector() {
